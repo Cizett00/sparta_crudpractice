b79e45eff2425a3b6c7b66850eb1035f6a13368f
Cizett00 <dltkdalsakak@gmail.com>
2026-05-15 16:34:23 +0900

    상품 테이블

--- src/main/java/com/sparta/practice/product/Product.java
+++ src/main/java/com/sparta/practice/product/Product.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 public class Product {
-    //테스트용 주석
+    //상품 테이블
 
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
